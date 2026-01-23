9457eaaee49ac12bed8e965816782bf92fbabbc0
Pradhan Atharva Ashish <e1337588@u.nus.edu>
2026-01-23 16:50:27 +0800

    Level-2

--- src/main/java/Adolf.java
+++ src/main/java/Adolf.java
@@ -3,31 +3,43 @@ import java.util.Scanner;
 public class Adolf {
     private static final String LINE =
             "____________________________________________________________";
+
     private static final String LOGO =
-            "   ___      ____    ___    _      _____ \\n"
-                    + "  / _ \\    |  _ \\  / _ \\  | |    |  ___|\\n"
-                    + " | |_| |   | | | || | | | | |    | |__  \\n"
-                    + " |  _  |   | | | || | | | | |    |  __| \\n"
-                    + " | | | |   | |_| || |_| | | |____| |___ \\n"
-                    + " |_| |_|   |____/  \\___/  |______|_____|\\n";
+            "   ___      ____    ___    _      _____ \n"
+                    + "  / _ \\    |  _ \\  / _ \\  | |    |  ___|\n"
+                    + " | |_| |   | | | || | | | | |    | |__  \n"
+                    + " |  _  |   | | | || | | | | |    |  __| \n"
+                    + " | | | |   | |_| || |_| | | |____| |___ \n"
+                    + " |_| |_|   |____/  \\___/  |______|_____|\n";
 
     public static void main(String[] args) {
-        printGreeting();
-
         Scanner sc = new Scanner(System.in);
+
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
+        printGreeting();
 
         while (true) {
             String input = sc.nextLine();
+            String cleaned = input.trim();
 
-            if (input.equals("bye")) {
+            if (cleaned.equals("bye")) {
                 printBox("Bye. Hope to see you again soon!");
                 break;
             }
 
-            printBox(input);
+            if (cleaned.equals("list")) {
+                printList(tasks, taskCount);
+                continue;
+            }
+
+            tasks[taskCount] = cleaned;
+            taskCount++;
+
+            printBox("added: " + cleaned);
         }
     }
-
 
     private static void printGreeting() {
         System.out.println(LINE);
@@ -40,6 +52,14 @@ public class Adolf {
     private static void printBox(String message) {
         System.out.println(LINE);
         System.out.println(" " + message);
+        System.out.println(LINE);
+    }
+
+    private static void printList(String[] tasks, int count) {
+        System.out.println(LINE);
+        for (int i = 0; i < count; i++) {
+            System.out.println(" " + (i + 1) + ". " + tasks[i]);
+        }
         System.out.println(LINE);
     }
 }
