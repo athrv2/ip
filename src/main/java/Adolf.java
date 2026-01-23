2e8c5f2953fe476b8c79b11a8791e23ec25a80a0
Pradhan Atharva Ashish <e1337588@u.nus.edu>
2026-01-23 16:33:42 +0800

    Level-1

--- src/main/java/Adolf.java
+++ src/main/java/Adolf.java
@@ -1,14 +1,45 @@
+import java.util.Scanner;
+
 public class Adolf {
+    private static final String LINE =
+            "____________________________________________________________";
+    private static final String LOGO =
+            "   ___      ____    ___    _      _____ \\n"
+                    + "  / _ \\    |  _ \\  / _ \\  | |    |  ___|\\n"
+                    + " | |_| |   | | | || | | | | |    | |__  \\n"
+                    + " |  _  |   | | | || | | | | |    |  __| \\n"
+                    + " | | | |   | |_| || |_| | | |____| |___ \\n"
+                    + " |_| |_|   |____/  \\___/  |______|_____|\\n";
+
     public static void main(String[] args) {
-        String line = "_____________________________";
-        String logo =
-                "   ___      ____    ___    _      _____ \\n"
-                        + "  / _ \\    |  _ \\  / _ \\  | |    |  ___|\\n"
-                        + " | |_| |   | | | || | | | | |    | |__  \\n"
-                        + " |  _  |   | | | || | | | | |    |  __| \\n"
-                        + " | | | |   | |_| || |_| | | |____| |___ \\n"
-                        + " |_| |_|   |____/  \\___/  |______|_____|\\n";
-        System.out.println(line + "\n" +  "Hello! I'm Adolf\n" + "What can I do for you?\n"
-                              + line + "\n" + "Bye. Hope to see you again soon!\n" + line);
+        printGreeting();
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String input = sc.nextLine();
+
+            if (input.equals("bye")) {
+                printBox("Bye. Hope to see you again soon!");
+                break;
+            }
+
+            printBox(input);
+        }
+    }
+
+
+    private static void printGreeting() {
+        System.out.println(LINE);
+        System.out.println(LOGO);
+        System.out.println("Hello! I'm Adolf");
+        System.out.println("What can I do for you?");
+        System.out.println(LINE);
+    }
+
+    private static void printBox(String message) {
+        System.out.println(LINE);
+        System.out.println(" " + message);
+        System.out.println(LINE);
     }
 }
