d67cae0808deba879d6e80a84f0c0fab40c5f0c5
Pradhan Atharva Ashish <e1337588@u.nus.edu>
2026-01-23 15:24:35 +0800

    Level-0

---
+++ src/main/java/Adolf.java
@@ -0,0 +1,14 @@
+public class Adolf {
+    public static void main(String[] args) {
+        String line = "_____________________________";
+        String logo =
+                "   ___      ____    ___    _      _____ \\n"
+                        + "  / _ \\    |  _ \\  / _ \\  | |    |  ___|\\n"
+                        + " | |_| |   | | | || | | | | |    | |__  \\n"
+                        + " |  _  |   | | | || | | | | |    |  __| \\n"
+                        + " | | | |   | |_| || |_| | | |____| |___ \\n"
+                        + " |_| |_|   |____/  \\___/  |______|_____|\\n";
+        System.out.println(line + "\n" +  "Hello! I'm Adolf\n" + "What can I do for you?\n"
+                              + line + "\n" + "Bye. Hope to see you again soon!\n" + line);
+    }
+}
